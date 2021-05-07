cc0c9eff79fd426f69c8c6c4d1e82c9fadd99596
Kim-Quirk <kimblackaz@gmail.com>
2021-05-07 14:36:57 -0600

    Responsive

--- lesson3/scripts/page.js
+++ lesson3/scripts/page.js
@@ -1,3 +1,12 @@
+window.addEventListener('load', () => {
+    const hambutton = document.querySelector('.ham');
+    const mainnav = document.querySelector('#navigation');
+
+    hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+    //window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+})
+
 window.addEventListener('load', (event)=> {
     const lu = document.querySelector('#lastupdated');
     lu.textContent = document.lastModified;
